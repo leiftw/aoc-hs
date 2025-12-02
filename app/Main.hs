f721017ea35f8517057116061564cf2d4d903a37
leiftw <leiftw8@icloud.com>
2025-12-02 16:16:02 +0100

    move some general stuff to library

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main (main) where
 
-import Data.Maybe (listToMaybe)
 import Data.Char (isDigit)
 import Data.List (nub)
 
@@ -23,10 +22,6 @@ main = do
 parseRot :: String -> Int
 parseRot ('L':r) = 0 - read r
 parseRot ('R':r) =     read r
-
--- TODO: unintuitive order, either postpend or `foldr` with initial `reverse`?
-accum :: Foldable fo => (b -> a -> b) -> b -> fo a -> [b]
-accum f i as = snd $ foldl' (\(c,ras) a -> (f c a,(f c a):ras)) (i,[]) as
 
 hits :: Int -> Int -> Int
 hits old rot = abs (rot`quot`100) + fromEnum (((old`div`100 /= new`div`100)
@@ -51,15 +46,3 @@ silliesInRange times (from,to) | (l`mod`times/=0) = silliesInRange times (intenn
        h = read hs :: Integer
        --hh = read (hs++hs)
        --step = intenner (l`div`times) + 1
-
-intenner :: Int -> Integer
-intenner i = read ('1':replicate i '0') --10^^l
-
--- from `Utils.ReadPMaybe`
-tryParser :: ReadP a -> String -> Maybe a
-tryParser = fmap fst . listToMaybe . reverse .|. readP_to_S
--- from `Utils.Util`
-infixr 8 .|. -- in (f .|. g . h) precedence does not matter
-             --    (f . g .|. h) does not make sense
-(.|.) :: (c -> d) -> (a -> b -> c) -> a -> b -> d -- rename?
-f .|. g = (f .) . g
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Maybe (listToMaybe)
+
+import Text.ParserCombinators.ReadP
+
+-- TODO: unintuitive order, either postpend or `foldr` with initial `reverse`?
+accum :: Foldable fo => (b -> a -> b) -> b -> fo a -> [b]
+accum f i as = snd $ foldl' (\(c,ras) a -> (f c a,(f c a):ras)) (i,[]) as
+
+intenner :: Int -> Integer
+intenner i = read ('1':replicate i '0') --10^^l
+
+-- from `Utils.ReadPMaybe`
+tryParser :: ReadP a -> String -> Maybe a
+tryParser = fmap fst . listToMaybe . reverse .|. readP_to_S
+
+-- from `Utils.Util`
+infixr 8 .|. -- in (f .|. g . h) precedence does not matter
+             --    (f . g .|. h) does not make sense
+(.|.) :: (c -> d) -> (a -> b -> c) -> a -> b -> d -- rename?
+f .|. g = (f .) . g
